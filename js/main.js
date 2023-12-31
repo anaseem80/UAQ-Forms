e8ead6c5a901f225d8b80c7d56a8efaae7f68e83
anaseem80 <anaseemamin@gmail.com>
2023-12-31 03:43:27 +0200

    12/29/2023, Home, Solutions, Gallery

--- js/main.js
+++ js/main.js
@@ -75,3 +75,29 @@ document.addEventListener('DOMContentLoaded', function() {
 	});
 	
 })(jQuery); 
+
+
+window.SmoothScrollOptions = {
+	// Scrolling Core
+	animationTime: 700, // [ms]
+	stepSize: 80, // [px]
+
+	// Acceleration
+	accelerationDelta: 50, // 50
+	accelerationMax: 3, // 3
+
+	// Keyboard Settings
+	keyboardSupport: true, // option
+	arrowScroll: 50, // [px]
+
+	// Pulse (less tweakable)
+	// ratio of "tail" to "acceleration"
+	pulseAlgorithm: true,
+	pulseScale: 4,
+	pulseNormalize: 1,
+
+	// Other
+	touchpadSupport: false, // ignore touchpad by default
+	fixedBackground: true,
+	excluded          : ''  
+}
