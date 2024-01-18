b9c74e48956b9b0fbcdcb698be5c8e330b51b0bf
Magdy Elsayyad - FR <magdy.elsayyad@eshaam.com>
2024-01-19 01:42:38 +0200

    FEAT: add DB connection

--- config/db.php
+++ config/db.php
@@ -1,11 +1,13 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'uaq-forms');
+define('DB_HOST', 'sikkatbeirutcom.ipagemysql.com'); // 'localhost'
+define('DB_USER', 'uaqforms');  // 'root'
+define('DB_PASS', 'Root@123456'); // ''
+define('DB_NAME', 'uaqforms'); // 'uaq-forms'
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if ($conn->connect_error) {
   die('Connection failed: ' . $conn->connect_error);
 }
+
+
